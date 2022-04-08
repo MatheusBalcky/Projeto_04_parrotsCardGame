9bb0af1f3239f17f498a48e089fd6129dd15c48c
Matheus Melo <matheuscabal821@gmail.com>
2022-04-07 22:12:17 -0300

    Random Cards Distribution

--- script/script.js
+++ script/script.js
@@ -1,4 +1,17 @@
 let cards = Number(prompt("Com quantas cartas você deseja jogar? (De 4 à 14, apenas números pares)"));
+
+let cardGif4 = ["bobrossparrot.gif","bobrossparrot.gif","explodyparrot.gif","explodyparrot.gif"];
+let cardGif6 = ["bobrossparrot.gif","bobrossparrot.gif","explodyparrot.gif","explodyparrot.gif","fiestaparrot.gif","fiestaparrot.gif"];
+let cardGif8 = ["bobrossparrot.gif","bobrossparrot.gif","explodyparrot.gif","explodyparrot.gif","fiestaparrot.gif","fiestaparrot.gif","metalparrot.gif","metalparrot.gif"];
+let cardGif10 = ["bobrossparrot.gif","bobrossparrot.gif","explodyparrot.gif","explodyparrot.gif","fiestaparrot.gif","fiestaparrot.gif","metalparrot.gif","metalparrot.gif","revertitparrot.gif","revertitparrot.gif"];
+let cardGif12 = ["bobrossparrot.gif","bobrossparrot.gif","explodyparrot.gif","explodyparrot.gif","fiestaparrot.gif","fiestaparrot.gif","metalparrot.gif","metalparrot.gif","revertitparrot.gif","revertitparrot.gif","tripletsparrot.gif","tripletsparrot.gif"];
+let cardGif14 = ["bobrossparrot.gif","bobrossparrot.gif","explodyparrot.gif","explodyparrot.gif","fiestaparrot.gif","fiestaparrot.gif","metalparrot.gif","metalparrot.gif","revertitparrot.gif","revertitparrot.gif","tripletsparrot.gif","tripletsparrot.gif","unicornparrot.gif","unicornparrot.gif"];
+
+
+
+function comparador(){
+    return Math.random() - 0.5;
+}
 function selectAmountCards (){ // função de seleção das cartãoc contendo a condicional para o jogo começar
     while (cards < 4 || cards % 2 !== 0 || cards > 14){
     alert("Número inválido digita novamente!");
@@ -7,24 +20,84 @@ function selectAmountCards (){ // função de seleção das cartãoc contendo a 
     alert("Número válidado bom jogo!");
 }
 function addCards (){ // função para add os cartões no canva do jogo
-    for(let i = 0; i < cards; i++){
-        document.querySelector(".cards").innerHTML +=
-        `
-            <div class="card" onclick="clickCard(this)">
-                <img class="front"style="width: 100px;" src="./images/front.png" alt="parrot-img">
-                <img class="back hidden" style="width: 100px;" src="./images/gifs/metalparrot.gif" alt="parrot-img">
-            </div>
-        `
+    if (cards === 4){
+        cardGif4.sort(comparador);
+        for(let i = 0; i < cards; i++){
+            document.querySelector(".cards").innerHTML +=
+            `
+                <div class="card ${cardGif4[i].replace(".gif","")}" onclick="clickCard(this)">
+                    <img class="front"style="width: 100px;" src="./images/front.png" alt="parrot-img">
+                    <img class="back hidden" style="width: 100px;" src="./images/gifs/${cardGif4[i]}" alt="parrot-img">
+                </div>
+            `
+        }
+    } else if (cards === 6){
+        cardGif6.sort(comparador);
+        for(let i = 0; i < cards; i++){
+            document.querySelector(".cards").innerHTML +=
+            `
+                <div class="card" onclick="clickCard(this)">
+                    <img class="front"style="width: 100px;" src="./images/front.png" alt="parrot-img">
+                    <img class="back hidden" style="width: 100px;" src="./images/gifs/${cardGif6[i]}" alt="parrot-img">
+                </div>
+            `
+        }
+    } else if (cards === 8){
+        cardGif8.sort(comparador);
+        for(let i = 0; i < cards; i++){
+            document.querySelector(".cards").innerHTML +=
+            `
+                <div class="card" onclick="clickCard(this)">
+                    <img class="front"style="width: 100px;" src="./images/front.png" alt="parrot-img">
+                    <img class="back hidden" style="width: 100px;" src="./images/gifs/${cardGif8[i]}" alt="parrot-img">
+                </div>
+            `
+        }
+    } else if (cards === 10){
+        cardGif10.sort(comparador);
+        for(let i = 0; i < cards; i++){
+            document.querySelector(".cards").innerHTML +=
+            `
+                <div class="card" onclick="clickCard(this)">
+                    <img class="front"style="width: 100px;" src="./images/front.png" alt="parrot-img">
+                    <img class="back hidden" style="width: 100px;" src="./images/gifs/${cardGif10[i]}" alt="parrot-img">
+                </div>
+            `
+        }
+    } else if (cards === 12){
+        cardGif12.sort(comparador);
+        for(let i = 0; i < cards; i++){
+            document.querySelector(".cards").innerHTML +=
+            `
+                <div class="card" onclick="clickCard(this)">
+                    <img class="front"style="width: 100px;" src="./images/front.png" alt="parrot-img">
+                    <img class="back hidden" style="width: 100px;" src="./images/gifs/${cardGif12[i]}" alt="parrot-img">
+                </div>
+            `
+        }
+    } else if (cards === 14){
+        cardGif14.sort(comparador);
+        for(let i = 0; i < cards; i++){
+            document.querySelector(".cards").innerHTML +=
+            `
+                <div class="card" onclick="clickCard(this)">
+                    <img class="front"style="width: 100px;" src="./images/front.png" alt="parrot-img">
+                    <img class="back hidden" style="width: 100px;" src="./images/gifs/${cardGif14[i]}" alt="parrot-img">
+                </div>
+            `
+        }
     }
 }
-function randomCards(){
-    
-}
+let cardClicked = [];
 function clickCard(elemento){ // fazer o flip no cartão e trocar de imagem
-    console.log(elemento);
     elemento.classList.toggle("flip");
     elemento.querySelector(".front").classList.toggle("hidden");
     elemento.querySelector(".back").classList.toggle("hidden");
+    cardClicked.push(elemento);
+    if (cardClicked.length > 1){
+        
+    }
+    console.log(cardClicked);
 }
 
 selectAmountCards();
